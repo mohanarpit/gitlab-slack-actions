d99b2ace9728b317bd3a01501d7976ee447c9665
Arpit Mohan <mohanarpit@users.noreply.github.com>
2019-10-05 22:19:35 +0530

    Migrating to use Slack client instead of axios. Cleaner code. (#12)
    Everything except for the view submission error is working properly
    Fixes #4

--- express.js
+++ express.js
@@ -1,9 +1,8 @@
 const dialog = require('./slack-api.js')
 const express = require('express')
 const bodyParser = require('body-parser')
-const gitlabApi = require('./gitlab-api.js')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,18 +25,19 @@ app.post('/slack/interactions', async (req, res) => {
     var response = ""
     switch(type) {
         case "message_action": 
-             response = await dialog.view(payload, res)
+            console.log("Going to process new view msg")
+            response = await dialog.view_new(payload)
             break
         case "view_submission":
-            response = await dialog.submit(payload, res)
+            console.log("Going to process view submission")
+            response = await dialog.submit_new(payload, res)
             break
-        
     }
-    console.log("Going to send response: " + JSON.stringify(response))
+    console.log("Going to send response to Slack: " + JSON.stringify(response))
     res.type("application/json; charset=utf-8")
     res.send(JSON.stringify(response))
 });
 
 app.listen(port, async () => {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`Gitlab Slack Actions app listening on port ${port}!`)
 })
