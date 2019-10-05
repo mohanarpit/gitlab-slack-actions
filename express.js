b9decb1e1735563f56acf173ebe4757fce317e69
Arpit Mohan <me@arpitmohan.com>
2019-10-05 20:57:49 +0530

    Updating the view after the gitlab issue is submitted via Slack Also minor refactoring

--- express.js
+++ express.js
@@ -18,12 +18,23 @@ app.post('/slack/events', (req, res) => {
     }
 })
 
-app.post('/slack/interactions', (req, res) => {
-    
+app.post('/slack/interactions', async (req, res) => {
     console.log(req.body.payload)
     payload = JSON.parse(req.body.payload)
-    dialog.view(payload, res)
-    res.send('OK!')
+    type = payload.type
+    var response = ""
+    switch(type) {
+        case "message_action": 
+             response = await dialog.view(payload, res)
+            break
+        case "view_submission":
+            response = await dialog.submit(payload, res)
+            break
+        
+    }
+    console.log("Going to send response: " + JSON.stringify(response))
+    res.type("application/json; charset=utf-8")
+    res.send(JSON.stringify(response))
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
